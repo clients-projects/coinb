07e426f79fdf282698704c1762197ccaa3a5ee4e
Igboanugwo Collins <munisco12@gmail.com>
2021-09-01 15:12:15 +0100

    added font

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -4,11 +4,11 @@ import {BiChevronDown} from 'react-icons/bi'
 const NavItem = ({ name, link_path, span }) => {   
 
     return (
-        <li className='py-3 px-6 lg:px-4 text-black lg:text-[#aaaaaa] lg:hover:text-white  lg:self-center font-medium text-lg flex justify-between '>
+        <li className='py-3 px-6 lg:px-4  lg:self-center font-medium text-lg flex justify-between '>
             <a href={link_path}>
                 {name}{' '}
                 {span ? (
-                    <span className='uppercase font-bold text-xs rounded-lg text-white bg-[#129DFF] px-2'>
+                    <span className='uppercase font-bold text-xs rounded-lg text-white px-2'>
                         {span}
                     </span>
                 ) : (
