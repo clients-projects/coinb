c8a7b2ed4cf3c747e866e80e620dede6d365dbc3
Igboanugwo Collins <munisco12@gmail.com>
2021-09-01 17:15:11 +0100

    working on nav

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -4,7 +4,7 @@ import { AiFillCaretDown } from 'react-icons/ai'
 const NavItem = ({ name, link_path, span }) => {   
 
     return (
-        <li className='py-3 px-6 lg:px-4  lg:self-center font-semibold text-xs flex justify-between '>
+        <li className='py-3 px-6 lg:px-4  lg:self-center font-semibold text-base flex justify-between '>
             <a href={link_path}>
                 {name}{' '}
                 
