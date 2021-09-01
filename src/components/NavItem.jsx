005381ae9db0ca5729b4afee551b6ebbd782c031
Igboanugwo Collins <munisco12@gmail.com>
2021-09-01 17:12:51 +0100

    working on nav

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -7,13 +7,7 @@ const NavItem = ({ name, link_path, span }) => {
         <li className='py-3 px-6 lg:px-4  lg:self-center font-semibold text-xs flex justify-between '>
             <a href={link_path}>
                 {name}{' '}
-                {span ? (
-                    <span className='uppercase font-bold text-xs rounded-lg text-white px-2'>
-                        {span}
-                    </span>
-                ) : (
-                    ''
-                )}
+                
             </a>
             {name === 'Products' ? (
                 <AiFillCaretDown className='self-center' />
