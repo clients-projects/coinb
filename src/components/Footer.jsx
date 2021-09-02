6f0bb34c5e3e021f7e144450830867c500be76d8
Igboanugwo Collins <munisco12@gmail.com>
2021-09-02 16:17:52 +0100

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <ul className='font-Helvetica text-5xl flex'>
-            <li>Forgot password?</li>
+            <li className='flex'>Forgot password?</li>
             <li>Don't have an account?</li>
             <li>Privacy Policy</li>
             <li>Have an issue with 2-factor authentication?</li>
