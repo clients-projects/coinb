4ad5aebfd265e75872c047d8138ad8c03a5ac35b
Igboanugwo Collins <munisco12@gmail.com>
2021-09-02 14:42:24 +0100

    working on nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -61,9 +61,10 @@ const Nav = () => {
                     </ul>
                     <NavItem name='Help' link_path='/' />
                 </ul>
-                <hr className='mt-3 border-t border-[#ffffff26]' />
-                <div className='ml-4'>
 
+                <hr className='mt-3 border-t border-[#ffffff26] pt-6 pb-3' />
+
+                <div className='ml-4'>
             <NavItem name='Prices' link_path='/' />
                 </div>
             </div>
