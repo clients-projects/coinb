e35833c2fbb3a2c500427e380046887f62f3c07b
Igboanugwo Collins <munisco12@gmail.com>
2021-09-02 05:46:28 +0100

    working on nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
     }
 
     let hamburgerStyle = (
-        <div className='flex z-30 relative border border-white p-1 '>
+        <div className='flex z-30 relative border border-white p-1 rounded-sm'>
             <BiMenu
                 className='text-white self-center text-2xl relative cursor-pointer'
                 onClick={toggleIsOn}
