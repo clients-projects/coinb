a048181743d6bfbf3c49b6a293affa6cb55abab7
Igboanugwo Collins <munisco12@gmail.com>
2021-09-02 21:41:18 +0100

    redesigning layout

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,7 +8,7 @@ import Footer from '../components/Footer'
 function Layout(props) {
     return (
         <div className=' grid h-screen overflow-x-hidden justify-items-center'>
-            <div className='grid w-full px-4 lg:w-11/12'>
+            <div className='grid w-full lg:px-4 lg:w-11/12'>
                 <div className='my-0 mx-auto w-full sm:pb-20'>
                     <Nav />
                 </div>
