78022afba057612f08ea2a482f10bac7f1a44685
Igboanugwo Collins <munisco12@gmail.com>
2021-09-02 05:43:37 +0100

    working on nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
     }
 
     let hamburgerStyle =  (
-        <div className='flex z-30 relative border border-white p-2 '>
+        <div className='flex z-30 relative border border-white p-1 '>
 
             <AiOutlineMenu
                 className='text-white self-center text-2xl relative cursor-pointer'
