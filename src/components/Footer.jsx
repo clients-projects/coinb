a7118586feb16952df824bd7dd9f81d17c4bab57
Igboanugwo Collins <munisco12@gmail.com>
2021-09-02 16:00:53 +0100

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <ul>
-            <li>Forgot password</li>
+            <li>Forgot password?</li>
             <li>Don't have an account?</li>
             <li></li>
             <li></li>
