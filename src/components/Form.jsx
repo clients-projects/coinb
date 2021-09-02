9448d56b6c4a22797913f9e6758e15799f031015
Igboanugwo Collins <munisco12@gmail.com>
2021-09-02 21:00:58 +0100

    removed unnecessary packages

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -76,7 +76,7 @@ const Form = (props) => {
                 </label>
             </div>
 
-            <button className='bg-gray-300 text-[#1652f0] justify-self-center py-2 px-6 font-semibold rounded-md outline-none sm:mb-5'>
+            <button className='justify-self-center py-2 px-6 font-semibold rounded-md outline-none sm:mb-5 btnclaim'>
                 CLAIM REWARD
             </button>
         </form>
